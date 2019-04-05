12f9b7f4181f3b1d7efb7e8721ae14d2b7e1422f
dyscalculia94 <nikola.blagojevic94@yahoo.com>
2019-04-05 11:21:42 +0200

    Designed the first level

--- scripts/CrateDest.gd
+++ scripts/CrateDest.gd
@@ -7,11 +7,11 @@ func get_area_type():
 	return "CRATE_DEST"
 
 func _on_CrateDest_area_entered(area):
-	get_parent().crates_num -= 1;
+	get_parent().get_parent().crates_num -= 1;
 #	print("CRATE_DEST_ENTERED")
 	area.switch_texture()
 
 func _on_CrateDest_area_exited(area):
-	get_parent().crates_num += 1;
+	get_parent().get_parent().crates_num += 1;
 #	print("CRATE_DEST_EXITED")
 	area.switch_texture()
--- scripts/Level1.gd
+++ scripts/Level1.gd
@@ -3,9 +3,10 @@ extends "Level.gd"
 var crates_num = 0
 
 func _ready():
-	for i in self.get_children():
-		if i.get_area_type() == "CRATE":
-			crates_num += 1
+	crates_num = get_child(0).get_children().size()
+#	for i in self.get_children():
+#		if i.get_area_type() == "CRATE":
+#			crates_num += 1
 	print("Number of crates on the level: " + str(crates_num))
 
 func _process(delta):
